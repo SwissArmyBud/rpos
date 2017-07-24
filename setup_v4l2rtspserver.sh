b4382026209782da44d18cd51ae0c3e8449d8553
AndrewP <swissarmybud@gmail.com>
2017-07-24 16:13:07 -0700

    Added pre-compiled V4L2rtspserver and modified install script

--- setup_v4l2rtspserver.sh
+++ setup_v4l2rtspserver.sh
@@ -1,7 +1,2 @@
-sudo apt install -y liblivemedia-dev liblog4cpp5-dev cmake libasound2-dev git
-git clone https://github.com/mpromonet/v4l2rtspserver
-cd v4l2rtspserver
-cmake . && make
-sudo make install
-cd ..
+sudo cp v4l2rtspserver /usr/bin/
 cp v4l2ctl.default.json v4l2ctl.json 
